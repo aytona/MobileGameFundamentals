6f990e905feeda010af21da538142b9eec8f5e9c
aytona <christopher.aytona@gmail.com>
2016-01-27 22:27:47 -0500

    Lab2 Added

---
+++ Lab2/Assets/Scripts/Ball.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+	
+	public float force = 100f;
+
+	Rigidbody2D bouncyBall;
+
+	void Start() {
+		bouncyBall = gameObject.GetComponent<Rigidbody2D> ();
+	}
+
+	public void ReceiveInput(Vector2 theSwipe) {
+		bouncyBall.AddForce (force * theSwipe.normalized);
+	}
+}
