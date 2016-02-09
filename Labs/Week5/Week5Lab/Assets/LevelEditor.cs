eb1049ac3b3d813b03c6c353c63eccadc07c35cc
Tech <tech@Techs-iMac-106.local>
2016-02-09 17:29:59 -0500

    Lab 4 completed

---
+++ Labs/Week5/Week5Lab/Assets/LevelEditor.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class LevelEditor : EditorWindow {
+
+	public string LevelName = "Level 1";
+	public Sprite GroundSprite;
+	public Sprite WaterSprite;
+	public float MaxGroundSprites = 1f;
+	public float MaxWaterSprites = 1f;
+
+	[MenuItem("Lab4/LevelEditor")]
+	public static void OpenLevelEditorWindow()
+	{
+		EditorWindow.GetWindow<LevelEditor> (true, "Level Editor");
+	}
+
+	void OnGUI ()
+	{
+		GUILayout.Label ("Level Name", EditorStyles.boldLabel);
+		LevelName = GUILayout.TextField (LevelName, 20);
+
+		GUILayout.Label ("Ground Sprite", EditorStyles.boldLabel);
+		GroundSprite = EditorGUILayout.ObjectField (GroundSprite, typeof(Sprite), true) as Sprite;
+
+		GUILayout.Label ("Water Sprite", EditorStyles.boldLabel);
+		WaterSprite = EditorGUILayout.ObjectField (WaterSprite, typeof(Sprite), true) as Sprite;
+
+		GUILayout.Label (MaxGroundSprites + "Max Ground Sprites", EditorStyles.boldLabel);
+		MaxGroundSprites = GUILayout.HorizontalScrollbar (MaxGroundSprites, 1.0f, 0.0f, 20.0f);
+		MaxGroundSprites = (int)MaxGroundSprites;
+
+		GUILayout.Label (MaxWaterSprites + "Max Water Sprites", EditorStyles.boldLabel);
+		MaxWaterSprites = GUILayout.HorizontalScrollbar (MaxWaterSprites, 1.0f, 0.0f, 20.0f);
+		MaxWaterSprites = (int)MaxWaterSprites;
+
+		if (GUILayout.Button ("Create Level"))
+		{
+			if (MaxGroundSprites == 0 || MaxWaterSprites == 0) 
+			{
+				ShowNotification (new GUIContent ("The numbers must be nonzero"));
+				return;
+			}
+			if (GroundSprite == null || WaterSprite == null)
+			{
+				ShowNotification (new GUIContent ("Please choose the sprites"));
+				return;
+			}
+			if (GroundSprite.bounds.size.y != WaterSprite.bounds.size.y)
+			{
+				ShowNotification (new GUIContent ("Sprites must be of same height"));
+				return;
+			}
+
+			CreateLevel(LevelName, GroundSprite, WaterSprite, MaxGroundSprites, MaxWaterSprites);
+		}
+	}
+
+	public static void CreateLevel (string name, Sprite sp1, Sprite sp2, float maxSp1, float maxSp2)
+	{
+		float xGround = sp1.bounds.size.x;
+		float xWater = sp2.bounds.size.x;
+		Vector3 currentLocation = new Vector3 (0, 0, 0);
+		int objectCount = 0;
+		int groundTiles = (int)Random.Range (0, maxSp1);
+		int waterTiles = (int)Random.Range (0, maxSp2);
+
+		GameObject rootObject = new GameObject ();
+		rootObject.name = name;
+
+		for (int i = 0; i < groundTiles; i++)
+		{
+			GameObject levelObject = new GameObject();
+			levelObject.transform.SetParent(rootObject.transform);
+			levelObject.name = name + objectCount;
+			objectCount++;
+			levelObject.transform.position = currentLocation;
+			currentLocation.x += xGround;
+			SpriteRenderer levelRenderer = levelObject.AddComponent<SpriteRenderer>();
+			levelRenderer.sprite = sp1;
+			levelObject.AddComponent<BoxCollider2D>();
+		}
+	}
+}
---
+++ Labs/Week5/Week5Lab/Assets/SpriteTiler.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class SpriteTiler : EditorWindow {
+
+	public string TileSpriteRootGameObjectName = "Tiled Object";
+
+	public float GridXSlider = 1f;
+	public float GridYSlider = 1f;
+
+	public Sprite TileLevelSprite;
+
+	[MenuItem("Week4 /Sprite Tiler")]
+	public static void OpenSpriteTileWindow() {
+		EditorWindow.GetWindow<SpriteTiler> (true, "Sprite Tiler");
+	}
+
+
+
+	void OnGUI() {
+		
+		GUILayout.Label ("Tile level object name", EditorStyles.boldLabel);
+		TileSpriteRootGameObjectName = 
+			GUILayout.TextField (TileSpriteRootGameObjectName, 30);
+		GUILayout.Label ("X:" + GridXSlider, EditorStyles.boldLabel);
+		GridXSlider = GUILayout.HorizontalScrollbar (GridXSlider, 1.0f, 0.0f, 30.0f);
+		GridXSlider = (int)GridXSlider;
+		GUILayout.Label ("Y:" + GridYSlider, EditorStyles.boldLabel);
+		GridYSlider = GUILayout.HorizontalScrollbar (GridYSlider, 1.0f, 0.0f, 30.0f);
+		GridYSlider = (int)GridYSlider;
+		GUILayout.Label ("Sprite level file", EditorStyles.boldLabel);
+		TileLevelSprite = EditorGUILayout.ObjectField (TileLevelSprite, typeof(Sprite), true) as Sprite;
+
+		if (GUILayout.Button ("Create Level")) {
+			if (GridXSlider == 0 && GridYSlider == 0) {
+				ShowNotification (new GUIContent ("Must have either X or Y > 0"));
+				return;
+			}
+			CreateSpriteTiledGameObject (GridXSlider, GridYSlider, 
+				TileLevelSprite, TileSpriteRootGameObjectName);
+		}
+
+	}
+
+
+	public static void CreateSpriteTiledGameObject(float GridXSlider, float GridYSlider,
+		Sprite SpriteLevelFile, string RootObjectName) {
+
+		// store the size of the sprite
+		float spriteX = SpriteLevelFile.bounds.size.x;
+		float spriteY = SpriteLevelFile.bounds.size.y;
+
+		GameObject rootObject = new GameObject ();
+		rootObject.transform.position = new Vector3 (0.0f, 0.0f, 0.0f);
+		rootObject.name = RootObjectName;
+
+		int currentObjectCount = 0;
+		int currentColumn = 0;
+		int currentrow = 0;
+		Vector3 currentLocation = new Vector3 (0.0f, 0.0f, 0.0f);
+
+		while (currentrow < GridYSlider) {
+			GameObject gridObject = new GameObject();
+			gridObject.transform.SetParent (rootObject.transform);
+			gridObject.name = RootObjectName + "_" + currentObjectCount;
+			gridObject.transform.position = currentLocation;
+
+			SpriteRenderer gridRenderer = 
+				gridObject.AddComponent<SpriteRenderer> ();
+			gridRenderer.sprite = SpriteLevelFile;
+			gridObject.AddComponent<BoxCollider2D> ();
+			currentLocation.x = currentLocation.x + spriteX;
+			//currentLocation.y = currentLocation.y + spriteY;
+			currentColumn++;
+
+			if (currentColumn >= GridXSlider) {
+				currentColumn = 0;
+				currentrow++;
+				currentLocation.x = 0;
+				currentLocation.y = currentLocation.y - spriteY;
+
+			}
+			currentObjectCount++;
+		}
+
+
+
+
+
+
+
+	}
+
+}
