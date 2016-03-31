b11947a234a2a972d34fc20bef1d30cf67583fbb
aytona <christopher.aytona@gmail.com>
2016-03-30 23:40:24 -0400

    Lab 8 added

---
+++ Labs/Lab8/Assets/Scripts/UIManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIManager : MonoBehaviour {
+	public void Pause() {
+		if (Time.timeScale == 1) {
+			Time.timeScale = 0;
+		} else if (Time.timeScale == 0) {
+			Time.timeScale = 1;
+		}
+	}
+}
